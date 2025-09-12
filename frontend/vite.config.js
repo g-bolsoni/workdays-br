a3dc29085510e012b6e58438c55b5d6addaa8d2e
Giovane Souza <giovane.essado@gmail.com>
2025-09-12 16:47:15 -0300

    feat: add ngrok.yml file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
+    allowedHosts: [
+      '3bb7e10ab528.ngrok-free.app'
+    ],
     proxy: {
       '/calculate': 'http://localhost:3001',
       '/calcular': 'http://localhost:3001',
